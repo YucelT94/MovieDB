4b4976212704c6ffb856e74caa970c85d3f1d404
YucelT94 <yucelterlemezuglu@gmail.com>
2019-01-16 20:59:39 +0300

    ProfileFragment edited

--- app/src/main/java/com/yucelt/moviedb/ui/ProfileFragment.java
+++ app/src/main/java/com/yucelt/moviedb/ui/ProfileFragment.java
@@ -6,15 +6,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.yucelt.moviedb.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProfileFragment extends Fragment {
     private static final String TAG = "ProfileFragment";
 
+    @BindView(R.id.imageViewProfile)
+    ImageView imageViewProfile;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this, view);
+
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions = requestOptions.transforms(new RoundedCorners(32));
+
+        Glide.with(getActivity())
+                .load(R.drawable.profile)
+                .apply(RequestOptions.circleCropTransform())
+                .into(imageViewProfile);
+
         return view;
     }
 
